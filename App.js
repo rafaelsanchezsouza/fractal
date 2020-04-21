e9e284db5ef91bf4d696c84e4ec213d61b4ec017
ssraf <rafaelsanchezsouza@gmail.com>
2020-04-21 17:26:17 -0300

    using react-native-svg

--- App.js
+++ App.js
@@ -1,50 +1,64 @@
-import React, { Component } from 'react';
-import Canvas from 'react-native-canvas';
-// import { StyleSheet, Text, View } from 'react-native';
-// import { WebView } from 'react-native-webview';
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  Text,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Image,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
 
-export default class App extends Component {
+/* Use this if you are using Expo
+import * as Svg from 'react-native-svg';
+const { Circle, Rect } = Svg;
+*/
 
-    handleCanvas = (canvas) => {
-      const ctx = canvas.getContext('2d');
-      ctx.fillStyle = 'purple';
-      ctx.fillRect(0,0,100,100);
-    }
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-    render() {
-      return (
-        <Canvas ref={this.handleCanvas}/>
-      )
-    }
+export default class SvgExample extends React.Component {
+  render() {
+    return (
+      <View
+        style={[
+          StyleSheet.absoluteFill,
+          { alignItems: 'center', justifyContent: 'center' },
+        ]}
+      >
+        <Svg height="50%" width="50%" viewBox="0 0 100 100">
+          <Circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="blue"
+            strokeWidth="2.5"
+            fill="green"
+          />
+          <Rect
+            x="15"
+            y="15"
+            width="70"
+            height="70"
+            stroke="red"
+            strokeWidth="2"
+            fill="yellow"
+          />
+        </Svg>
+      </View>
+    );
+  }
 }
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <WebView
-//         originWhitelist={['*']}
-//         source={{ 
-//           html: '<h1>Hello world</h1>' 
-//         }}
-//         style={{ marginTop: 20 }}
-//       />
-//     );
-//   }
-// }
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hello World!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
