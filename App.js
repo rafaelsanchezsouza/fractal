a36580dc36802fbd9c71595a99235e327eb06307
Rafael Sanchez Souza <ssraf@Rafaels-MacBook-Air.local>
2020-04-20 22:00:56 -0300

    canvas working

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Canvas from 'react-native-canvas';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+    handleCanvas = (canvas) => {
+      const ctx = canvas.getContext('2d');
+      ctx.fillStyle = 'purple';
+      ctx.fillRect(0,0,100,100);
+    }
+
+    render() {
+      return (
+        <Canvas ref={this.handleCanvas}/>
+      )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <WebView
+//         originWhitelist={['*']}
+//         source={{ 
+//           html: '<h1>Hello world</h1>' 
+//         }}
+//         style={{ marginTop: 20 }}
+//       />
+//     );
+//   }
+// }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello World!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
